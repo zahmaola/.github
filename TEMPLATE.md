61d28dc3d643e2e5837dd5222aad905ad35970b8
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-09-26 13:20:41 +0300

    attempt 1

---
+++ TEMPLATE.md
@@ -0,0 +1,11 @@
+# Zahmaola Organization
+
+This organization is for the Zahmaola project and codebases.
+
+## Repositories
+
+<!-- PLACE_HOLDER_FOR_REPOSITORIES -->
+
+## Thanks
+
+a lot
---
+++ generate-readme.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const githubToken = process.env.GITHUB_TOKEN;
+const orgName = process.env.ORG_NAME;  // Read from environment variable
+const url = `https://api.github.com/orgs/${orgName}/repos`;
+
+axios.get(url, { headers: { Authorization: `token ${githubToken}` } })
+.then(response => {
+    const repos = response.data.map(repo => `### [${repo.name}](${repo.html_url})\n ${repo.description || 'No description'}`).join('\n');
+
+    // Read TEMPLATE.md
+    const template = fs.readFileSync('./TEMPLATE.md', 'utf8');
+
+    // Insert the repo list into the template
+    const readmeContent = template.replace('<!-- PLACE_HOLDER_FOR_REPOSITORIES -->', repos);
+
+    // Write README.md
+    fs.writeFileSync('./profile/README.md', readmeContent);
+  })
+  .catch(error => console.error(error));
